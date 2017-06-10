e672a7880427f7d4c19958c3dbfd5da6515633d4
Michal Adamski <michal.adamski@igt.com>
2017-06-10 07:03:57 +0200

    Initial commit

---
+++ strategy/src/main/java/pl/adamskim/dp/App.java
@@ -0,0 +1,13 @@
+package pl.adamskim.dp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
